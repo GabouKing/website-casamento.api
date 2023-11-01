a31eeca20928a33ebf2d52fd3f09ab8853dd56b7
GabouKing <gribeirodelimas2@gmail.com>
2023-10-31 21:16:57 -0300

    Alterando banco de prisma para supabase

--- src/server.js
+++ src/server.js
@@ -1,50 +1,77 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { PrismaClient } = require('@prisma/client');
+const { createClient } = require('@supabase/supabase-js');
 const cors = require('cors');
 
 const app = express();
-const prisma = new PrismaClient();
+const supabaseUrl = 'https://pvgsasoytbxikvjflsnm.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2Z3Nhc295dGJ4aWt2amZsc25tIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5ODc5MDQ3OSwiZXhwIjoyMDE0MzY2NDc5fQ.xWY6UtGVE_czaHyfBffbyvTyX0QZtVAKmaLiVvonZcA';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 app.use(cors());
 app.use(bodyParser.json());
 
 // Rota para obter todos os presentes
 app.get('/presentes', async (req, res) => {
-  const presentes = await prisma.presente.findMany();
-  res.json(presentes);
+  let { data, error } = await supabase.from('Produtos').select('*');
+  console.log('data',data)
+  if (error) {
+    console.error('Erro ao buscar dados:', error);
+    res.status(500).json({ error: 'Erro ao buscar dados' });
+    return;
+  }
+  res.json(data);
 });
 
 // Rota para criar um novo presente
 app.post('/presentes', async (req, res) => {
   const { urlFoto, urlProduto, nomeProduto, comprado } = req.body;
-  const presente = await prisma.presente.create({
-    data: {
+  const { data, error } = await supabase.from('produtos').upsert([
+    {
       urlFoto,
       urlProduto,
       nomeProduto,
       comprado,
     },
-  });
-  res.json(presente);
+  ]);
+  if (error) {
+    console.error('Erro ao criar presente:', error);
+    res.status(500).json({ error: 'Erro ao criar presente' });
+    return;
+  }
+  res.json(data[0]);
 });
 
 // Rota para atualizar um presente
 app.put('/presentes/:id', async (req, res) => {
   const { id } = req.params;
   const { urlFoto, urlProduto, nomeProduto, comprado } = req.body;
-  const updatedPresente = await prisma.presente.update({
-    where: { id: parseInt(id) },
-    data: { urlFoto, urlProduto, nomeProduto, comprado },
-  });
-  res.json(updatedPresente);
+  const { data, error } = await supabase.from('produtos').upsert([
+    {
+      id: parseInt(id),
+      urlFoto,
+      urlProduto,
+      nomeProduto,
+      comprado,
+    },
+  ]);
+  if (error) {
+    console.error('Erro ao atualizar presente:', error);
+    res.status(500).json({ error: 'Erro ao atualizar presente' });
+    return;
+  }
+  res.json(data[0]);
 });
 
 // Rota para deletar um presente
 app.delete('/presentes/:id', async (req, res) => {
   const { id } = req.params;
-  await prisma.presente.delete({
-    where: { id: parseInt(id) },
-  });
+  const { error } = await supabase.from('produtos').delete().eq('id', parseInt(id));
+  if (error) {
+    console.error('Erro ao deletar presente:', error);
+    res.status(500).json({ error: 'Erro ao deletar presente' });
+    return;
+  }
   res.json({ message: 'Presente deletado com sucesso.' });
 });
 
